005ce667b641e39516c5fc914e4c790b70693333
Momokh99 <mohamedkhamekhami@gmail.com>
2025-07-10 23:00:42 +0100

    Update Color.kt

---
+++ app/src/main/java/com/momokh99/launcher/ui/theme/Color.kt
@@ -0,0 +1,27 @@
+package com.momokh99.launcher.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Light Theme Colors
+val md_theme_light_primary = Color(0xFF2196F3)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFFBBDEFB)
+val md_theme_light_onPrimaryContainer = Color(0xFF1976D2)
+val md_theme_light_secondary = Color(0xFF03A9F4)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_background = Color(0xFFFAFAFA)
+val md_theme_light_onBackground = Color(0xFF121212)
+val md_theme_light_surface = Color(0xFFFFFFFF)
+val md_theme_light_onSurface = Color(0xFF121212)
+
+// Dark Theme Colors
+val md_theme_dark_primary = Color(0xFF90CAF9)
+val md_theme_dark_onPrimary = Color(0xFF0D47A1)
+val md_theme_dark_primaryContainer = Color(0xFF1976D2)
+val md_theme_dark_onPrimaryContainer = Color(0xFFBBDEFB)
+val md_theme_dark_secondary = Color(0xFF81D4FA)
+val md_theme_dark_onSecondary = Color(0xFF01579B)
+val md_theme_dark_background = Color(0xFF121212)
+val md_theme_dark_onBackground = Color(0xFFE0E0E0)
+val md_theme_dark_surface = Color(0xFF1E1E1E)
+val md_theme_dark_onSurface = Color(0xFFE0E0E0)
