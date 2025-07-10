551e281fd20bcc2b3a3f3e5ddef33c848bacbd9c
Momokh99 <mohamedkhamekhami@gmail.com>
2025-07-11 00:35:26 +0100

    Update Branch.kt

--- app/src/main/java/com/momokh99/launcher/data/Branch.kt
+++ app/src/main/java/com/momokh99/launcher/data/Branch.kt
@@ -2,12 +2,15 @@ package com.momokh99.launcher.data
 
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+
 
 data class Branch(
     val name: String,
-    val apps: List<AppInfo>,
-    val color: Color,
-    val icon: ImageVector? = null
+    val apps: List<AppInfo> = mutableListOf(),
+    val color: Color = Color.Blue,
+    val icon: ImageVector = Icons.Default.Folder
 )
 package com.momokh99.launcher.data
 
